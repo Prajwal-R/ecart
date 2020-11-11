4afa749e8ec8425e67415a590ec58c08287f1ba2
prajwal <prajwalbhat30@gmail.com>
2020-11-12 00:11:51 +0530

    updated 11_11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {BrowserRouter, Route,Link} from 'react-router-dom';
 import './App.css';
-import data from './data';
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
 function App() {
 
 const openMenu = () =>{
@@ -10,13 +12,12 @@ const closeMenu = () =>{
   document.querySelector(".sidebar").classList.remove("open")
 }
   return (
+    <BrowserRouter>
     <div className="grid-container">
     <header className="header">
       <div className="brand">
-        <button onClick={openMenu}>
-          &#9776;
-        </button>
-        <a href="index.html">Esell</a>
+      <button onClick={openMenu}>&#9776;</button>
+      <Link to ="/">Esell</Link>
       </div>
       <div className="header-links">
         <a href="cart.html">Cart</a>
@@ -39,25 +40,9 @@ const closeMenu = () =>{
     </aside>
     <main className="main">
       <div className="content">
-        <ul className="products">
-          {
-            data.products.map (product =>
-              <li>
-                <div className="product">
-                  <img className="product-image" src={product.image} alt="product" />
-                  <div className="product-name">
-                    <a href="product.html">{product.name}</a>
-                  </div>
-                  <div className="product-brand">{product.brand}</div>
-                  <div className="product-price">Inr {product.price}</div>
-                  <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
-                </div>
-              </li>)
-            }
-          
-          
-
-        </ul>
+      <Route path ="/product/:id" component={ProductScreen}/>
+      <Route path = "/" exact={true} component={HomeScreen}/>
+        
       </div>
 
     </main>
@@ -65,6 +50,7 @@ const closeMenu = () =>{
       All right reserved.
     </footer>
   </div>
+  </BrowserRouter>
   );
 }
 
